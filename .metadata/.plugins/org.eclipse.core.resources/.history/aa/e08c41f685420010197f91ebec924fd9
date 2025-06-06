eeef11791963baf8678506755161f347633ac291
Nam Le <nam.le@s3corp.com.vn>
2025-06-06 14:14:03 +0700

    Sơ lược về TestNG

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/aa/e08c41f685420010197f91ebec924fd9
@@ -0,0 +1,19 @@
+package firstpackage;
+
+import org.testng.annotations.Test;
+
+public class secondclass {
+	@Test
+	public void FirstMethod()
+	{
+		System.out.println("This is first method of secondclass.");
+	}
+	
+	@Test
+	public void SecondtMethod()
+	{
+		System.out.println("This is Second Method of secondclass");
+	}
+	
+
+}
--- FirstTestNGProject/src/secondpackage/firstclass.java
+++ FirstTestNGProject/src/secondpackage/firstclass.java
@@ -1,5 +1,18 @@
 package secondpackage;
 
+import org.testng.annotations.Test;
+
 public class firstclass {
+	@Test
+	public void FirstMethod()
+	{
+		System.out.println("This is first method of first class - secondpackage");
+	}
+	
+	@Test
+	public void SecondtMethod()
+	{
+		System.out.println("This is Second Method of first class - secondpackage");
+	}
 
 }
